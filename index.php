14a43e8011f59194fe23b4806d7a01bad78af1bb
Shubham Sharma <shubhamsharma.in0@gmail.com>
2020-04-19 22:26:47 +0530

    index.php

--- index.php
+++ index.php
@@ -26,7 +26,7 @@
   <body>
 		<div class="login">
 			<h1>Login</h1>
-			<form action="/php/login.php" method="POST">
+			<form action="php/login.php" method="POST">
 				<label for="username">
 					<i class="fas fa-user"></i>
 				</label>
